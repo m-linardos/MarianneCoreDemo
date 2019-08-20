0330bb6bc759e40aadd1b5c6c835ce4875c25d1d
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-20 10:34:44 -0400

    added MyDb class

---
+++ MarianneCoreDemo/Models/MyDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarianneCoreDemo.Models
+{
+    public class MyDB : DbContext      //MyDB inherits from DbContext //using statement
+    {
+        public DbSet<Product> Products  { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ MarianneCoreDemo/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarianneCoreDemo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [MaxLength(80)]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        [MaxLength(8)]
+        public string Status { get; set; }
+    }
+}
