60e9f81a9da1b679333688ca481e38f3eaf9cf0a
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-20 10:12:29 -0400

    Add project files.

---
+++ MarianneCoreDemo/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarianneCoreDemo.Models
+{
+    public class Customer
+    {
+        // properties
+        [Key]
+        public int CustomerId { get; set; }
+
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime Added { get; set; }
+
+        public decimal CreditLimit { get; set; }
+    }
+}
